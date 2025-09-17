6e131e7d123f4e03720db200725fdcea46562b9c
Erik <erikpersson0884@gmail.com>
2025-09-17 17:33:08 +0200

    switched from jest to vitest for frontend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 import icons from './pwa/icons.json'
@@ -14,6 +14,11 @@ export default defineConfig({
                 secure: true,
             },
         }
+    },
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./tests/setupTests.ts",
     },
     plugins: [
         react(),
