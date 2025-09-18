54b753d04796bda5753bba6ff67145975f3aa467
Erik <erikpersson0884@gmail.com>
2025-09-18 19:16:05 +0200

    added special page icons for special people

--- client/src/layout/header/Header.tsx
+++ client/src/layout/header/Header.tsx
@@ -3,13 +3,33 @@ import './Header.css';
 import vevITLogo from '../../assets/vevit-white.png';
 import AuthButton from '../../components/authButton/AuthButton';
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../../contexts/authContext';
+import pixlefiedLogo from '../../assets/header-images/pixlefied-logo.svg';
+import gooseicon from '../../assets/header-images/goose.png';
+import ideicon from '../../assets/header-images/ide.png';
+import fredagicon from '../../assets/header-images/fredag.png';
+import neineiicon from '../../assets/header-images/neinei.png';
 
 const Header: React.FC = () => {
+    const { currentUser } = useAuthContext();
+    const [headerLogo, setHeaderLogo] = React.useState<string>(vevITLogo);
 
+    React.useEffect(() => {
+    const username = currentUser?.username?.toLowerCase();
+        if (username === 'neinei') setHeaderLogo(neineiicon);
+        else if (username === 'pixeln') setHeaderLogo(pixlefiedLogo);
+        else if (username === 'goose') setHeaderLogo(gooseicon);
+        else if (username === 'ide') setHeaderLogo(ideicon);
+        else if (username === 'fredag') setHeaderLogo(fredagicon);
+        else setHeaderLogo(vevITLogo);
+
+        console.log(currentUser?.username);
+    }, [currentUser]);
+    
     return (
         <header className="page-header">
             <Link to="/">
-                <img src={vevITLogo} alt="VevIT Logo" className="header-logo" width={200}/>
+                <img src={headerLogo} alt="VevIT Logo" className="header-logo" width={200}/>
             </Link>
             <AuthButton />
         </header>
