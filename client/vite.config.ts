4ec12c68e5f5fc692f81fc3f172bba7960b8765a
Erik <erikpersson0884@gmail.com>
2025-10-09 11:21:22 +0200

    made VITE_BASE_URL obsolete

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,9 +9,9 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: 'http://localhost:3001/api',
+                target: 'http://localhost:3001/',
                 changeOrigin: true,
-                secure: true,
+                secure: false,
             },
         }
     },
